95a957554ba643d8a2b978ef85415ea1cea1d1f6
Sado1111 <sadohannahfabohns@gmail.com>
2023-08-26 06:01:03 +0100

    craeted assembly code that prints Hello Holberton

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -3,8 +3,11 @@ hello_msg db "Hello, Holberton", 0
 
 section .text
 global main
+extern printf
 
 main:
-mov rdi, hello_msg
-call printf
+sub rsp, 8            ; Align the stack
+mov rdi, hello_msg   ; Load the address of hello_msg into rdi
+call printf          ; Call the printf function
+add rsp, 8            ; Restore the stack
 ret
