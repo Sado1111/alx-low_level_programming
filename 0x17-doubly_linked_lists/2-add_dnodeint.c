207dfac38d670a7790010c7d372a7e4fc26ec1df
Sado1111 <sadohannahfabohns@gmail.com>
2024-01-25 20:59:38 -0800

    betty and gcc compliant for task 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,16 +14,20 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	new_node = malloc(sizeof(dlistint_t));
 
 	if (new_node == NULL)
+	{
 		return (NULL);
+	}
 	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = NULL;
 	if (*head == NULL)
+	{
 		*head = new_node;
 		return (new_node);
+	}
 	new_node->next = *head;
 	(*head)->prev = new_node;
-	*head = new_node
+	*head = new_node;
 
 	return (new_node);
 }
