65458419a0e5fc9ad19e88f920fe090f8d0e6b64
Sado1111 <sadohannahfabohns@gmail.com>
2024-01-25 20:08:35 -0800

    became betty compliant and created a function that frees nodes in a list

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -1,12 +1,17 @@
 #include "lists.h"
 
 /**
- *
- *
- *
- *
+ * free_dlistint - frees all the nodes in the list
+ * @head: pointer to the head/first node
  */
 void free_dlistint(dlistint_t *head)
 {
+	dlistint_t *temp;
 
+	while (head != NULL)
+	{
+		temp = head;
+		free(temp);
+		head = head->next;
+	}
 }
